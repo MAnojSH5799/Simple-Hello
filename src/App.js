daae7345d903580a95d2beb62159e04ea949e4fd
swap009 <swanil009gmail.com>
2022-07-20 11:31:01 +0530

    simple hello-3

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  const [count, setCount] = useState("")
+  const IncNum = () => {
+    setCount("Hello World")
+  }
+  return (
+    <div className="App">
+    <h1>{count}</h1>
+<button className='bg-primary' onClick={IncNum}>Click</button>
+{/* onclick={() => setCount(count + 1)} */}
+    </div>
+  );
+}
+
+export default App;
