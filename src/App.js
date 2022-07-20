678470f12143275ef8b0b41475be5f6bc31ff5a6
du04 <https://github.com/du04/Aagu-App.git>
2022-07-20 18:07:13 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
-import React, { useState, useEffect } from 'react';
-// import logo from './logo.svg';
-import './App.css';
-
-
+import React, { useState, useEffect } from "react";
+import {Row,Col,Button,Container} from 'react-bootstrap';
+import Add from './Components/Add'
+import Sub from './Components/Sub'
+import Mul from './Components/Mul'
+import Div from './Components/Div'
 
 function App() {
-  const [count, setCount] = useState("")
-  const IncNum = () => {
-    setCount("Hello World")
+  const [changes, setChange]=useState(false);
+  const [val, setVal]=useState(0);
+
+  const onClick=(str)=>{
+   setChange(!changes)
+   if(str==='add')
+    setVal(1)
+    else if(str==='sub')
+    setVal(2)
+    else if(str==='mul')
+    setVal(3)
+    else if(str==='div')
+    setVal(4)
   }
+  
   return (
-    <div className="App">
-    <h1>{count}</h1>
-<button className='bg-primary' onClick={IncNum}>Click</button>
-{/* onclick={() => setCount(count + 1)} */}
-    </div>
+    <Container className="bg-info" style={{height:'400px', width:'400px'}}>
+    {!changes?
+    <>
+      <h1 className="text-center">
+        Buttons
+      </h1>
+      <Row className="m-5">
+        <Col xs={3} sm={3} md={3} lg={3} xl={3} xxl={3}>
+          <Button onClick={()=>onClick('add')}>Add</Button>
+        </Col>
+        <Col xs={3} sm={3} md={3} lg={3} xl={3} xxl={3}>
+          <Button onClick={()=>onClick('sub')}>Sub</Button>
+        </Col>
+        <Col xs={3} sm={3} md={3} lg={3} xl={3} xxl={3}>
+          <Button onClick={()=>onClick('mul')}>Mul</Button>
+        </Col>
+        <Col xs={3} sm={3} md={3} lg={3} xl={3} xxl={3}>
+          <Button onClick={()=>onClick('div')}>Div</Button>
+        </Col>
+      </Row>
+    </>
+    :
+    val===1?
+      <Add/>
+      :
+      val==2?
+        <Sub/>
+        :val===3?
+          <Mul/>
+          :
+          <Div/>
+    }
+
+    <Button onClick={onClick}>Back</Button>
+    </Container>
   );
 }
 
---
+++ src/Components/Div.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import {Row,Col,Button,Container,Input} from 'react-bootstrap';
+
+function App() {
+  const [a, setA]=useState(0);
+  const [b, setB]=useState(0);
+  const [c, setC]=useState(0);
+
+  const onClick=(x,y)=>{
+     setC(+a / +b);
+  }
+  const onChangeA=(e)=>{
+    setA(e.target.value)
+   }
+
+   const onChangeB=(e)=>{
+    setB(e.target.value)
+   }
+  
+  return (
+    <>
+    <h1 className="text-center">
+        Div
+    </h1>
+    <h4>
+        Enter numbers:
+    </h4>
+    <input onChange={onChangeA} className="m-2"/>
+    <input onChange={onChangeB} className="m-2"/>
+
+    <Button onClick={onClick} className="m-2">Div</Button>
+
+    <h5>Results : {c}</h5> 
+    </>
+  );
+}
+
+export default App;
